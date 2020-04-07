2e479fe322cc3e2883b301e083668f7af995c135
dmuino <dmuinoo@gmail.com>
2020-04-07 08:11:15 +0200

    Deploy fix7

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from foodtaskerapp import views
 from django.contrib.auth import views as auth_views
@@ -10,6 +10,9 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+
+    # Restaurant
+
     url(r'^restaurant/sign-in/$', auth_views.login, 
     	{'template_name': 'restaurant/sign_in.html'},
     	name= 'restaurant-sign-in'),
@@ -18,5 +21,8 @@ urlpatterns = [
     	name = 'restaurant-sign-out'),
     url(r'^restaurant/sign-up', views.restaurant_sign_up, 
     	name= 'restaurant-sign-up'),
-    url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home')
+    url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home'),
+
+    # Sign In / Sign Up / Sign Out
+    url(r'api/social/', include('rest_framework_social_oauth2.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
